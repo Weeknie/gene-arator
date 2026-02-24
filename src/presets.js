81c7ba7bf95017d691b67803db1418c5db3af586
Maarten Slenter <maartenslenter@gmail.com>
2026-02-24 21:10:25 +0100

    Add rainbow cycle 2 and newlines in presets

--- src/presets.js
+++ src/presets.js
@@ -1,11 +1,15 @@
 export const presets = [
   {
     name: 'Rainbow cycle',
-    code: 'R+25;(R>200)->G+25;(G>200)->B+25;(B>200)->R-25'
+    code: 'R+25;\n(R>200)->G+25;\n(G>200)->B+25;\n(B>200)->R-25'
   },
   {
+    name: 'Rainbow cycle 2',
+    code: 'R+25;\n(R>200)->G+50;(R>200)->B-100;\n(G>200)->B+50;(G>200)->R-100;\n(B>200)->R+50;(B>200)->G-100;'
+  }
+  {
     name: 'Self sustaining cells',
-    code: 'R-25;G-25;B-25;(R>50)->R+100;(G>50)->G+100;(B>50)->B+100'
+    code: 'R-25;\nG-25;\nB-25;\n(R>50)->R+100;\n(G>50)->G+100;\n(B>50)->B+100'
   }
 ];
 
